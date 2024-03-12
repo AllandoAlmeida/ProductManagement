44406e8ae278c06734620b4e949bce5d5fece360
Alando <alandoalmeida@hotmail.com>
2024-03-12 12:17:42 -0300

    feat: create ProductModel

--- src/product/ProductModel.java
+++ src/product/ProductModel.java
@@ -1,5 +1,37 @@
 package product;
 
 public class ProductModel {
+    private String barCode; 
+    private String name;
+    private int priceInCents;
+    private int stock; 
+
+    public String getBarCode() {
+        return this.barCode;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getPriceInCents() {
+        return this.priceInCents;
+    }
+
+    public Integer getStock() {
+        return this.stock;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setPriceInCents(Integer newPriceInCents) {
+        this.priceInCents = newPriceInCents;
+    }
+
+    public void setStock(Integer newStock) {
+        this.stock = newStock;
+    }
     
 }
