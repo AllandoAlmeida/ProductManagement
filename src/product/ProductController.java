d34717b977ff890369dbfc91343211003878fe7b
Alando <alandoalmeida@hotmail.com>
2024-03-12 12:18:01 -0300

    feat: Create ProductController

--- src/product/ProductController.java
+++ src/product/ProductController.java
@@ -1,5 +1,36 @@
 package product;
 
+import java.util.ArrayList;
+
+import exceptions.NegativePriceException;
+import exceptions.NotFoundException;
+
 public class ProductController {
-    
+    private ArrayList<ProductModel> products = new ArrayList<>();
+
+    public String create(ProductModel payload) throws Exception {
+
+        if (payload.getPriceInCents() < 0) {
+            throw new NegativePriceException("O preço do produto precisa ser um número positivo.");
+        }
+
+        products.add(payload);
+
+        return "Produto '" + payload.getName() + "' adicionado.";
+    }
+
+    public ArrayList<ProductModel> read() {
+        return products;
+    }
+
+    public Integer retrievePrice(String barCode) throws Exception {
+
+        for (ProductModel product : products) {
+            if (product.getBarCode().equals(barCode)) {
+                return product.getPriceInCents();
+            }
+        }
+
+        throw new NotFoundException("Produto com código de barras '" + barCode + "' não encontrado.");
+    }
 }
